fb16ea58da6977892f54fe6a4d690afc3b75f74c
Chris Barrett <chris.d.barrett@me.com>
2016-12-10 21:46:03 +1300

    Add dir-locals file

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c-mode
+  (flycheck-clang-include-path "/Users/chrisb/Code/tmk_keyboard/common" "/usr/local/Cellar/avr-libc/2.0.0/avr/include")))
