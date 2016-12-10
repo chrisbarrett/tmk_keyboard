46ca53e61f78cabce59d063376e9de74ddff442f
Chris Barrett <chris.d.barrett@me.com>
2016-12-10 21:52:45 +1300

    Make with mouse control support

--- upload.sh
+++ upload.sh
@@ -3,4 +3,8 @@
 set -e
 
 cd keyboard/atreus
-make upload KEYMAP=walrus_programmer_dvorak USB=/dev/cu.usbmodem1411
+
+make upload \
+    KEYMAP=walrus_programmer_dvorak \
+    USB=/dev/cu.usbmodem1411 \
+    MOUSEKEY_ENABLE=yes
