ab0d3847734b9359931dff758611f6b076edb2c9
Chris Barrett <chris.d.barrett@me.com>
2016-12-11 09:38:38 +1300

    Change `make upload` to search for device

--- upload.sh
+++ upload.sh
@@ -4,7 +4,4 @@ set -e
 
 cd keyboard/atreus
 
-make upload \
-    KEYMAP=walrus_programmer_dvorak \
-    USB=/dev/cu.usbmodem1411 \
-    MOUSEKEY_ENABLE=yes
+make upload KEYMAP=walrus_programmer_dvorak
