cf0d17f7e2b26e41903cfc807ca8d9ad473f3cae
Chris Barrett <chris.d.barrett@me.com>
2017-02-26 12:52:33 +1300

    Change to hardware dvorak

--- keyboard/atreus/keymap_walrus_programmer_dvorak.c
+++ keyboard/atreus/keymap_walrus_programmer_dvorak.c
@@ -1,58 +1,30 @@
 #include "keymap_common.h"
 
-/* Soft dvorak layout
- *
- */
-
-
-/* Mapping from Programmer Dvorak to qwerty.
- *
- * This is needed to account for the difference in symbol and number keys
- * between standard dvorak and Programmer Dvorak.
- */
-
-#define DV_EXCLMRK    KC_MINUS
-#define DV_AT         KC_RBRACKET
-#define DV_PCT        SHIFT(KC_1)
-#define DV_LBRKT      KC_3
-#define DV_RBRKT      KC_4
-#define DV_HASH       KC_EQUAL
-#define DV_CARET      SHIFT(KC_RBRACKET)
-#define DV_TILDE      SHIFT(KC_GRAVE)
-#define DV_DOLLAR     KC_GRAVE
-#define DV_ASTERISK   KC_7
-#define DV_PIPE       KC_BSLASH
-#define DV_BSLASH     SHIFT(KC_BSLASH)
-#define DV_LBRACE     KC_2
-#define DV_RBRACE     KC_0
-#define DV_LPAREN     KC_5
-#define DV_RPAREN     KC_8
-#define DV_AMP        KC_1
-#define DV_GRAVE      SHIFT(KC_EQUAL)
-#define DV_MINUS      KC_QUOT
-#define DV_SLASH      KC_LBRACKET
-#define DV_EQUAL      KC_6
-
-#define DV_1        SHIFT(KC_5)
-#define DV_2        SHIFT(KC_8)
-#define DV_3        SHIFT(KC_4)
-#define DV_4        SHIFT(KC_9)
-#define DV_5        SHIFT(KC_3)
-#define DV_6        SHIFT(KC_0)
-#define DV_7        SHIFT(KC_2)
-#define DV_8        SHIFT(KC_MINUS)
-#define DV_9        SHIFT(KC_6)
-#define DV_0        SHIFT(KC_7)
+#define KC_TILDE    SHIFT(KC_GRAVE)
+#define KC_AT       SHIFT(KC_2)
+#define KC_CARET    SHIFT(KC_6)
+#define KC_DOLLAR   SHIFT(KC_4)
+#define KC_PCT      SHIFT(KC_5)
+#define KC_ASTERISK SHIFT(KC_8)
+#define KC_EXCLMRK  SHIFT(KC_1)
+#define KC_AMP      SHIFT(KC_7)
+#define KC_LPAREN   SHIFT(KC_9)
+#define KC_RPAREN   SHIFT(KC_0)
+#define KC_LBRACE   SHIFT(KC_LBRACKET)
+#define KC_RBRACE   SHIFT(KC_RBRACKET)
+#define KC_PIPE     SHIFT(KC_BSLASH)
+#define KC_HASH     SHIFT(KC_3)
+#define KC_COLON    SHIFT(KC_SCLN)
 
 /* Keymaps */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* 0: qwerty */
-  KEYMAP(KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,    \
-         KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, \
-         KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, \
-         KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC, KC_FN4, KC_FN5,     \
-         KC_SPC, KC_FN0, DV_MINUS, DV_SLASH, KC_ENT),                   \
+  /* 0: letters */
+  KEYMAP(KC_COLON, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, \
+         KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S,    \
+         KC_QUOT, KC_Q, KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, \
+         KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,   \
+         KC_SPC, KC_FN0, KC_MINS, KC_SLSH, KC_ENT),                     \
 
   /* 1: fn with undvorak-ized punctuation
    *
@@ -61,14 +33,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *  \    |      }     )    ]        ||       `     1     2     3    -
    * L2  insert super   =  bkspc ctrl || alt space   fn    .     0    #
    */
-  KEYMAP(DV_TILDE, DV_AT, DV_CARET, DV_DOLLAR, DV_PCT,                  \
-         KC_PGUP, DV_7, DV_8, DV_9, DV_ASTERISK,                        \
-         DV_EXCLMRK, DV_AMP, DV_LBRKT, DV_LPAREN, DV_LBRACE,            \
-         KC_PGDN, DV_4, DV_5, DV_6, KC_KP_PLUS,                         \
-         DV_BSLASH, DV_PIPE, DV_RBRKT, DV_RPAREN, DV_RBRACE,            \
-         DV_GRAVE, DV_1, DV_2, DV_3, KC_KP_MINUS,                       \
-         KC_FN1, SHIFT(KC_INS), KC_LGUI, DV_EQUAL, KC_BSPC, KC_FN4, KC_FN5, \
-         KC_SPC, KC_FN0, KC_KP_DOT, DV_0, DV_HASH),                     \
+  KEYMAP(KC_TILDE, KC_AT, KC_CARET, KC_DOLLAR, KC_PCT,                  \
+         KC_PGUP, KC_7, KC_8, KC_9, KC_ASTERISK,                        \
+         KC_EXCLMRK, KC_AMP, KC_LBRACKET, KC_LPAREN, KC_LBRACE,         \
+         KC_PGDN, KC_4, KC_5, KC_6, KC_KP_PLUS,                         \
+         KC_BSLASH, KC_PIPE, KC_RBRACKET, KC_RPAREN, KC_RBRACE,         \
+         KC_GRAVE, KC_1, KC_2, KC_3, KC_KP_MINUS,                       \
+         KC_FN1, SHIFT(KC_INS), KC_LGUI, KC_EQUAL, KC_BSPC, KC_FN4, KC_FN5, \
+         KC_SPC, KC_FN0, KC_KP_DOT, KC_0, KC_HASH),                     \
 
   /* 2: arrows and function keys
    *
@@ -83,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_DOWN, KC_F4, KC_F5, KC_F6, KC_F11,                          \
          KC_FN22, KC__VOLUP, KC_NO, KC_NO, KC_FN3, KC_NO,               \
          KC_F1, KC_F2, KC_F3, KC_F12,                                   \
-         KC_NO, KC__VOLDOWN, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL,         \
+         KC_NO, KC__VOLDOWN, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL,        \
          KC_LALT, KC_SPC, KC_FN2, KC_NO, KC_NO, KC_ENT),
 
   /* 3: mouse control, reached by fn+esc, then holding z*/
